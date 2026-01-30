786f8c5c044ea5e280280fd0c60d8e49ff9d66ee
BLameui33 <alexanderdemirel@gmail.com>
2026-01-30 19:36:30 +0100

    cc

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 // script.js (FINALE VERSION FÜR DIE ÖFFENTLICHE WEBSEITE)
 
+function toggleFlipCards(sectionId) {
+    // 1. Checkbox Status prüfen
+    const checkbox = document.getElementById('toggle-' + sectionId);
+    const isChecked = checkbox.checked;
+
+    // 2. Grid finden
+    const grid = document.getElementById('grid-' + sectionId);
+    if (!grid) return;
+
+    // 3. Labels einfärben (Optional, für besseres UX)
+    const labelFront = document.getElementById('label-' + sectionId + '-front');
+    const labelBack = document.getElementById('label-' + sectionId + '-back');
+    
+    if (isChecked) {
+        labelFront.classList.remove('active');
+        labelBack.classList.add('active');
+    } else {
+        labelFront.classList.add('active');
+        labelBack.classList.remove('active');
+    }
+
+    // 4. Alle Karten drehen
+    const cards = grid.querySelectorAll('.flip-card-inner');
+    cards.forEach(card => {
+        if (isChecked) {
+            card.classList.add('is-flipped');
+        } else {
+            card.classList.remove('is-flipped');
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
  // Funktion für die Fade-In Animation beim Scrollen
  function setupScrollAnimation() {
@@ -254,37 +286,7 @@ function setupScrollHighlighting() {
     });
 }
 
-function toggleFlipCards(sectionId) {
-    // 1. Checkbox Status prüfen
-    const checkbox = document.getElementById('toggle-' + sectionId);
-    const isChecked = checkbox.checked;
 
-    // 2. Grid finden
-    const grid = document.getElementById('grid-' + sectionId);
-    if (!grid) return;
-
-    // 3. Labels einfärben (Optional, für besseres UX)
-    const labelFront = document.getElementById('label-' + sectionId + '-front');
-    const labelBack = document.getElementById('label-' + sectionId + '-back');
-    
-    if (isChecked) {
-        labelFront.classList.remove('active');
-        labelBack.classList.add('active');
-    } else {
-        labelFront.classList.add('active');
-        labelBack.classList.remove('active');
-    }
-
-    // 4. Alle Karten drehen
-    const cards = grid.querySelectorAll('.flip-card-inner');
-    cards.forEach(card => {
-        if (isChecked) {
-            card.classList.add('is-flipped');
-        } else {
-            card.classList.remove('is-flipped');
-        }
-    });
-}
 
 function setupCounterAnimation() {
     const counters = document.querySelectorAll('.metric-number');
